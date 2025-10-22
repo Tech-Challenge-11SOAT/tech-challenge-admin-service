5d31c871c32b717353efde8093b298370f97f124
Pedro Argentati <pedro.argentatii@gmail.com>
2025-10-22 19:47:49 -0300

    Initial commit

---
+++ src/main/java/br/com/postech/techchallange_admin/TechchallangeAdminApplication.java
@@ -0,0 +1,13 @@
+package br.com.postech.techchallange_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechchallangeAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechchallangeAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/postech/techchallange_admin/TechchallangeAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.postech.techchallange_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechchallangeAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
