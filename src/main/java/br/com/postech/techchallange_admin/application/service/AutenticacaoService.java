e958ecc330888b144e15b128208cb485821ae8d8
Endrew <endrewslvp@gmail.com>
2025-11-08 18:55:46 -0300

    feat(service): adiciona o servico de autenticacao implementando os UseCases

---
+++ src/main/java/br/com/postech/techchallange_admin/application/service/AutenticacaoService.java
@@ -0,0 +1,47 @@
+package br.com.postech.techchallange_admin.application.service;
+
+import br.com.postech.techchallange_admin.domain.model.Admin;
+import br.com.postech.techchallange_admin.domain.port.in.AutenticarAdminUseCase;
+import br.com.postech.techchallange_admin.domain.port.in.LogoutAdminUseCase;
+import br.com.postech.techchallange_admin.domain.port.out.AdminRepositoryPort;
+import br.com.postech.techchallange_admin.infrastructure.security.TokenBlacklistService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutenticacaoService implements AutenticarAdminUseCase, LogoutAdminUseCase {
+
+    private final AdminRepositoryPort adminRepositoryPort;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenBlacklistService tokenBlacklistService;
+
+    public AutenticacaoService(AdminRepositoryPort adminRepositoryPort,
+                               PasswordEncoder passwordEncoder,
+                               TokenBlacklistService tokenBlacklistService) {
+        this.adminRepositoryPort = adminRepositoryPort;
+        this.passwordEncoder = passwordEncoder;
+        this.tokenBlacklistService = tokenBlacklistService;
+    }
+
+    @Override
+    public Admin autenticar(String email, String senha) {
+
+        Admin admin = adminRepositoryPort.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Usuário ou senha inválidos"));
+
+        if (!passwordEncoder.matches(senha, admin.getSenhaHash())) {
+            throw new RuntimeException("Usuário ou senha inválidos");
+        }
+        return admin;
+    }
+
+    @Override
+    public void logout(String token) {
+        // Adiciona o token na "lista negra"
+        // (adaptado do AdminUserService.java do monolito)
+        if (token != null && token.startsWith("Bearer ")) {
+            token = token.substring(7);
+        }
+        tokenBlacklistService.blacklistToken(token);
+    }
+}
